944c7aff3d0d16caf4044a351c0f3a9559f5ea6f
Roozbeh-web <roozbeh279@gmail.com>
2022-08-01 16:55:44 +0430

    add time model and migration

---
+++ database/migrations/2022_08_01_122256_create_times_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->boolean('status');
+            $table->integer('interval');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+};
