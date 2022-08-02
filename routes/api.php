6e739d81336e00ba4e58122cd9d3d182eceee5f8
Roozbeh-web <roozbeh279@gmail.com>
2022-08-02 09:55:50 +0430

    remove useless moduels from routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 //public routes
 
