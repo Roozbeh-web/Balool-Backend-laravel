9ed94086cee58f2a0c98302f7f59ae4aea60682f
Roozbeh-web <roozbeh279@gmail.com>
2022-08-03 11:10:56 +0430

    add follows route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+Route::get('/follows', [UserController::class, 'follows']);
 
 
 //protected routes
