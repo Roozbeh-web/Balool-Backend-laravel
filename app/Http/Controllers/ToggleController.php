bc865d4c3a9b8b246d363328ca4ee0f515ed9102
Roozbeh-web <roozbeh279@gmail.com>
2022-08-10 12:59:35 +0430

    add toggle action

--- app/Http/Controllers/ToggleController.php
+++ app/Http/Controllers/ToggleController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Toggle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ToggleController extends Controller
 {
-    //
+    public function toggle(Request $request){
+        $id = Auth()->user()->id;
+
+        $validator = Validator::make($request->all(), [
+            'is_toggled' => 'required|boolean'
+        ]);
+
+        if($validator->fails()){
+            return Response([
+                'message' => $validator->messages(),
+            ]);
+        }else{
+            $toggle = Toggle::create([
+                'user_id' => $id,
+                'is_toggled' => $request->is_toggled
+            ]);
+
+            return Response([
+                'message' => 'toggled successfully.',
+                'toggle_info' => $toggle
+            ]);
+        }
+    }
 }
